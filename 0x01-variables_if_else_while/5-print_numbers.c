a8047805a95af159d70dce7ee991426311adbba9
ishsta <ishstaglass@yahoo.com>
2020-02-07 03:10:57 +0000

    print all single digit numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	int i;
+	char c, n;
+
+	c = '0';
+	n = '\n';
+
+	for (i = 0; i <10; i++)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar(n);
+	return (0);
+}
