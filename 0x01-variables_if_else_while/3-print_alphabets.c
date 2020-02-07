ae6a2b241f64ab4f39aed03a7eb7e5d5ce925e29
ishsta <ishstaglass@yahoo.com>
2020-02-07 01:45:48 +0000

    Lowercase then uppercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 'a';
+	int y = 'A';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
