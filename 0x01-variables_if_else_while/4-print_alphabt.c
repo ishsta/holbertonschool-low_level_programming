66aa249daedb0ee525069b241aca39bf74daff92
ishsta <ishstaglass@yahoo.com>
2020-02-07 01:53:16 +0000

    lowercase alphabet no q or e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 'a';
+
+	while (x <= 'z')
+	{
+		if ((x != 'e') && (x != 'q')0
+			{
+				putchar(x);
+				x++;
+			}
+			}
+		putchar('n');
+		return (0);
+	}
