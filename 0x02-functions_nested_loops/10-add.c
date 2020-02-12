4d93bb27b5d7598532d3e123f353f977c80ecf86
ishsta <ishstaglass@yahoo.com>
2020-02-12 06:08:58 +0000

    add'

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * add - Adds two values given as input and displays the sum
+ * @a: First integer
+ * @b: Second integer
+ * Return: Success
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
